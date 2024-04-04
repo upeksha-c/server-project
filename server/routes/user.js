47e216ceb9ec59f4796f0cf6cec40fb7211e004f
Gloryozo <gloryozoji@gmail.com>
2024-04-04 17:43:28 +0300

    both login and registeration are working fully well

--- server/routes/user.js
+++ server/routes/user.js
@@ -9,35 +9,47 @@ userRouter.post("/login",async(req,res) => {
     const sql = "select * from userinfo where email = $1"
     const result = await query(sql,[req.body.email])
     if (result.rowCount === 1) {
-
-      if (result.rows[0].password === req.body.password) {
-        res.status(200).json({id: result.rows[0].id})
+      bcrypt.compare(req.body.password,result.rows[0].password,(err,bcrypt_res) => {
+        if (!err) {
+          if (bcrypt_res === true) {
+            const user = result.rows[0]
+            res.status(200).json({"id":user.id,"email":user.email})
           } else {
             res.statusMessage = 'Invalid login'
             res.status(401).json({error: 'Invalid login'})
           }
-          } else {
+        } else {
+          res.statusMessage = err
+          res.status(500).json({error: err})
+        }
+      })
+    } else {
       res.statusMessage = 'Invalid login'
       res.status(401).json({error: 'Invalid login'})
     }
-    }catch (error) {
-      res.statusMessage = error
-      res.status(500).json({error: error})
-    }
+  } catch (error) {
+    res.statusMessage = error
+    res.status(500).json({error: error})
+  }
 })
 
 userRouter.post("/register",async(req,res) => {
-           try {
+    bcrypt.hash(req.body.password,10,async (err,hash) => {
+      if (!err) {
+        try {
           const sql = "insert into userinfo (email, password) values ($1,$2) returning id"
-          const result = await query(sql,[req.body.email,req.body.password])
+          const result = await query(sql,[req.body.email,hash])
           res.status(200).json({id: result.rows[0].id}) 
         } catch (error) {
           res.statusMessage = error
           res.status(500).json({error: error})
         }
-           })
-
-
+      } else {
+        res.statusMessage = err
+        res.status(500).json({error: err})
+      }
+    })
+})
 
 module.exports = {
   userRouter
