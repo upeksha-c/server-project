e69814886dbf669b3ecde130c5c801e4410c43b2
Upeksha Chiranthi <upekshachiranthi@gmail.com>
2024-04-08 21:43:07 +0300

    Change user.js query source file

--- server/routes/user.js
+++ server/routes/user.js
@@ -1,5 +1,5 @@
 const express = require('express')
-const { query } = require('../helpers/logindb.js')
+const { query } = require('../helpers/db.js')
 const bcrypt = require('bcrypt')
 
 const userRouter = express.Router()
