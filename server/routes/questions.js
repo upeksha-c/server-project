999170c80edaabeab882a16eaf42cf5e147c0a28
Upeksha Chiranthi <upekshachiranthi@gmail.com>
2024-04-21 10:03:35 +0300

    Correct a mistake

--- server/routes/questions.js
+++ server/routes/questions.js
@@ -18,7 +18,7 @@ questionsRouter.get("/questions",async(req,res) => {
 
 questionsRouter.get("/categories",async(req,res) => {
     try{
-        const result = await query('select cate_name, ,id_category from categories')
+        const result = await query('select cate_name, id_category from categories')
         const rows = result.rows ? result.rows : []
         res.status(200).json(rows)
     }catch(error){
