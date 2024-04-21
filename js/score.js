17c2ab52469e56f4ab3fd43526852bed1f39c8a0
Upeksha Chiranthi <upekshachiranthi@gmail.com>
2024-04-21 10:21:41 +0300

    Do changes to frontend to send score to database

--- js/score.js
+++ js/score.js
@@ -1,3 +1,8 @@
+import { Score } from "./class/score.js";
+import { User } from "./class/Userinfo.js";
+const user = new User();
+const scoreObj = new Score()
+
 // Function to get URL parameters
 const getParameterByName = (name, url = window.location.href) => {
     name = name.replace(/[\[\]]/g, "\\$&");
@@ -12,7 +17,10 @@ const getParameterByName = (name, url = window.location.href) => {
 const score = getParameterByName('score');
 const totalQuestions = getParameterByName('totalQuestions');
 const id_category = getParameterByName('categoryId');
-console.log(id_category)
+
+//get id of current logged user
+const id_user = user.id 
+
 
 // Display the score
 const scoreContainer = document.getElementById('container1');
@@ -25,3 +33,12 @@ const scoreHTML = `
   </div>
 `;
 scoreContainer.innerHTML = scoreHTML;
+
+//if user is logged in user score data send to database
+if(id_user){
+  scoreObj.sendScore(id_user,id_category,score).then(json => {
+    console.log(json)
+  }).catch(error => {
+    alert(error)
+  })
+}
