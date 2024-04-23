95188c109572e2666cd4249e258c4e94d5f290a5
Archana Ojha <archana.ozza@gmail.com>
2024-04-23 19:00:22 +0300

    made changes in feedback

--- server/routes/feedbacks.js
+++ server/routes/feedbacks.js
@@ -5,8 +5,8 @@ const fbRouter = express.Router() //handle routing for the feedback-related endp
 
 fbRouter.post("/feedback",async(req,res) => { //route handler for POST requests to the "/feedback" endpoint
   try {
-    const sql = "insert into feedback (user_id , email, feedback_text, satisfaction_rating ) values ($1,$2,$3,$4) returning id_feedback"
-    const result = await query(sql,[req.body.user_id,req.body.email,req.body.feedback_text, req.body.satisfaction_rating])
+    const sql = "insert into feedback (user_id , feedback_text, satisfaction_rating ) values ($1,$2,$3) returning id_feedback"
+    const result = await query(sql,[req.body.user_id,req.body.feedback_text, req.body.satisfaction_rating])
     res.status(200).json({id_feedback: result.rows[0].id_feedback}) 
   } catch (error) {
     res.statusMessage = error
