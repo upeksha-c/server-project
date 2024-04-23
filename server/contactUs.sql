b011cff8b76657479fcf386c592b4718bcc7bb9d
Upeksha Chiranthi <upekshachiranthi@gmail.com>
2024-04-23 18:00:56 +0300

    Add files to front end to handle contactus form submission

--- server/contactUs.sql
+++ server/contactUs.sql
@@ -2,9 +2,9 @@ CREATE TABLE inquiry (
     id_inquiry SERIAL PRIMARY KEY,
     first_name VARCHAR(100) NOT NULL,
     last_name VARCHAR(100) NOT NULL,
-    email VARCHAR(100) UNIQUE NOT NULL,
-    company VARCHAR(100) NOT NULL,
+    email VARCHAR(100) NOT NULL,
+    company VARCHAR(100),
     country VARCHAR(255) NOT NULL,
     phone NUMERIC NOT NULL,
-    message TEXT NOT NULL
+    message TEXT 
 );
