4d4e230099d2b91383b551af2da1e27556b8149a
Upeksha Chiranthi <upekshachiranthi@gmail.com>
2024-04-27 17:02:35 +0300

    Change score page content

--- js/score.js
+++ js/score.js
@@ -24,11 +24,33 @@ const id_user = user.id
 
 // Display the score
 const scoreContainer = document.getElementById('container1');
+//Assign encouragement statement according to score
+let encouragement = ""
+let image_source = ""
+if(score > 9){
+  encouragement = "Well done!! Congratulations!"
+  image_source = "../photos/congradualation.jpg"//downloaded from Freepik and a link to website (www.freepik.com)
+}
+else if(score > 5){
+  encouragement = "Good jobGood job! Keep up the great work!"
+  image_source = "../photos/good_job.jpg"//downloaded from Freepik and a link to website (www.freepik.com)
+}
+else if(score > 3){
+  encouragement = "Nice try! Keep it up!"
+  image_source = "../photos/good_job.jpg"//downloaded from Freepik and a link to website (www.freepik.com)
+}
+else{
+  encouragement = "Nice try! Try again!"
+  image_source = "../photos/try_again.jpg"//downloaded from Freepik and a link to website (www.freepik.com)
+}
+console.log(encouragement)
 const scoreHTML = `
   <div class="card">
     <div class="card-body">
       <h2 class="card-title">Quiz Result</h2>
-      <p class="card-text">You scored: ${score} out of ${totalQuestions}</p>
+      <img src = ${image_source} alt ="" class ='container-fluid'>
+      <p class ="greet">${encouragement}</p>
+      <p class="card-text">You scored: ${score} / ${totalQuestions}</p>
     </div>  
   </div>
 `;
