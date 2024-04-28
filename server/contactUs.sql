28a5169aa45b9ab0c8564f63e89f61ffb1209dc8
Upeksha Chiranthi <upekshachiranthi@gmail.com>
2024-04-28 13:45:35 +0300

    Change serverside files according to new layout of contactus page

--- server/contactUs.sql
+++ server/contactUs.sql
@@ -1,10 +1,8 @@
 CREATE TABLE inquiry (
     id_inquiry SERIAL PRIMARY KEY,
-    first_name VARCHAR(100) NOT NULL,
-    last_name VARCHAR(100) NOT NULL,
+    full_name VARCHAR(100) NOT NULL,
     email VARCHAR(100) NOT NULL,
-    company VARCHAR(100),
-    country VARCHAR(255) NOT NULL,
-    phone NUMERIC NOT NULL,
+	phone NUMERIC NOT NULL,
+    subject VARCHAR(100) NOT NULL,
     message TEXT 
 );
