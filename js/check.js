509166eb504c251f83e708846647d748d469efba
Upeksha Chiranthi <upekshachiranthi@gmail.com>
2024-05-03 11:52:08 +0300

    change

--- js/check.js
+++
@@ -1,9 +0,0 @@
-import { User } from "./class/Userinfo.js";
-const user = new User();
-
-
-const profile = document.querySelector('#profile')
-
-if(!user.isLoggedIn){
-    profile.style.display = 'none'
-}
--- js/logout.js
+++
@@ -1,9 +0,0 @@
-import { User } from "./class/Userinfo.js"
-
-const user = new User()
-//catch logout button from profile dropdown
-const login_link = document.querySelector('a#logout-button')
-
-
-//set eventlistener, when click on button it give logout option
-login_link.addEventListener('click',() => user.logout())
